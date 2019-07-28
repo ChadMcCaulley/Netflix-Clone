4969dc10c6fbd63c60402d0302e9eadc98208350
ChadMcCaulley <cha2168871@maricopa.edu>
2019-07-27 22:24:34 -0700

    Working on front page responsiveness

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Link} from "react-router-dom";
 
 import Logo_Full from "../resources/images/netflix_logo_full.png";
-import {FaChevronRight} from "react-icons/fa";
+import {IoIosArrowForward} from "react-icons/io";
 
 
 export default function Home() {
@@ -10,14 +10,14 @@ export default function Home() {
         <>
             <header className="landing-page-showcase">
                 <div className="landing-page-showcase-top">
-                    <img src={Logo_Full} alt="NETFLIX"/>
-                    <p className="btn-helper-text"> Have an account?</p>
-                    <Link to="/" className="btn btn-rounded">Sign In</Link>
+                    <Link to="/" ><img src={Logo_Full} alt="NETFLIX"id="logo"/></Link>
+                    <p> Have an account? </p>
+                    <Link to="/" className="btn btn-rounded"> Sign In</Link>
                 </div>
                 <div className="landing-page-showcase-content">
                     <h1> See what's next</h1>
                     <p> Watch anywhere. Cancel anytime</p>
-                    <Link to="/" className="btn btn-xl"> Try 30 Days Free <FaChevronRight/></Link>
+                    <Link to="/" className="btn btn-xl"> Try 30 Days Free <IoIosArrowForward id="signup-link-icon"/></Link>
                 </div>
             </header>
         </>
