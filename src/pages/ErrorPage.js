c35b364e2600e94012c12c5b3408513faaf10b5c
ChadMcCaulley <cha2168871@maricopa.edu>
2019-07-31 13:27:30 -0700

    Separating SCSS files. Error page now styled

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -6,7 +6,17 @@ const ErrorPage = () => {
     return (
         <div>
             <NavbarSimple/>
-            <Link to="/" className="text-xxl"> Go Back to Netflix </Link>
+            <div className="error-message-container">
+                <p className="text-xl text-heavy">Netflix Site Error - Page Not Found</p>
+                <p>
+                    We were unable to find that page.
+                </p>
+                <p>Please go to the Netflix home page by clicking the button below.
+                </p>
+                <Link to="/" className="btn btn-dark">
+                    Netflix Home
+                </Link>
+            </div>
         </div>
     )
 }
