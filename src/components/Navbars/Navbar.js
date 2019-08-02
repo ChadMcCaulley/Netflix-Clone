f666486c16df355632af9a75fe7e2ea0ef4c31c9
ChadMcCaulley <cha2168871@maricopa.edu>
2019-08-02 13:59:07 -0700

    fixed navbar. Login page more responsive on smaller screens

--- src/components/Navbars/Navbar.js
+++ src/components/Navbars/Navbar.js
@@ -4,13 +4,20 @@ import {Link} from "react-router-dom";
 import Logo_Full from "../../resources/images/netflix_logo_full.png";
 
 export default function Navbar({logoLink, pText, btnText, btnLink}) {
-    let btnClass = ""
+    let btnClass = "", logoClass = "";
     if(btnText !== undefined && btnText !== "") btnClass = "btn btn-rounded";
+    if((pText === "" || pText === undefined) && (btnText === "" || btnText === undefined)){
+        logoClass = "logo-alone";
+    }
     return (
         <nav className="nav-simple">
-            <Link to={logoLink}><img src={Logo_Full} alt="NETFLIX" id="logo"/></Link>
-            <p>{pText}</p>
-            <Link to={btnLink} className={btnClass}>{btnText}</Link>
+            <Link to={logoLink} className={logoClass}><img src={Logo_Full} alt="NETFLIX" id="logo"/></Link>
+            {pText !== "" && pText !== undefined && (
+                <p>{pText}</p>
+            )}
+            {btnText !== "" && btnText !== undefined && (
+               <Link to={btnLink} className={btnClass}>{btnText}</Link> 
+            )}
         </nav>
     )
 }
