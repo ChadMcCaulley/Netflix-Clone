43a86a5fe45078d9f4c0e222b499e616c516ca8a
ChadMcCaulley <cha2168871@maricopa.edu>
2019-08-03 14:03:17 -0700

    signup page 2 now reacts to user clicks

--- src/pages/SignupTourPage1.js
+++ src/pages/SignupTourPage1.js
@@ -6,30 +6,34 @@ import {FaCheckCircle, FaCheck} from "react-icons/fa";
 
 const SignupTourPage1 = () => {
     return (
-        <div className="signup-tour-container">
-            <NavbarLight logoLink="/" btnText="Sign In" btnLink="/signin-existing"/>
-            <form className="signup-tour-inner">
-                <div className="signup-tour-inner-top">
-                    <FaCheckCircle id="check-icon-circle" size={42}/>
-                    <p className="step-number">
-                        Step
-                        <strong> 1 </strong>
-                        of
-                        <strong> 3 </strong>
-                    </p>
-                    <p className="text-xl text-bold">
-                        Choose your plan</p>
-                </div>
-                <ul className="tour-list">
-                    <li>
-                        <FaCheck id="check-icon"/>You won't be charged until after your free month.</li>
-                    <li>
-                        <FaCheck id="check-icon"/>We'll remind you three days before your trial ends.</li>
-                    <li>
-                        <FaCheck id="check-icon"/>No commitments, cancel anytime.</li>
-                </ul>
-                <Link to="/signup-tour/plans" className="btn btn-lg">see the plans</Link>
-            </form>
+        <div>
+            <div className="white-background"></div>
+            <div className="signup-tour-container">
+                <NavbarLight logoLink="/" btnText="Sign In" btnLink="/signin-existing"/>
+                <form className="signup-tour-inner">
+                    <div className="signup-tour-inner-top">
+                        <FaCheckCircle id="check-icon-circle" size={42}/>
+                        <p className="step-number">
+                            Step <strong>
+                                1
+                            </strong> of <strong>
+                                3
+                            </strong>
+                        </p>
+                        <p className="text-xl text-bold">
+                            Choose your plan</p>
+                    </div>
+                    <ul className="tour-list">
+                        <li>
+                            <FaCheck id="check-icon"/>You won't be charged until after your free month.</li>
+                        <li>
+                            <FaCheck id="check-icon"/>We'll remind you three days before your trial ends.</li>
+                        <li>
+                            <FaCheck id="check-icon"/>No commitments, cancel anytime.</li>
+                    </ul>
+                    <Link to="/signup-tour/plans" className="btn btn-lg">see the plans</Link>
+                </form>
+            </div>
         </div>
     )
 }
--- src/pages/SignupTourPage2.js
+++ src/pages/SignupTourPage2.js
@@ -4,6 +4,12 @@ import NavbarLight from "../components/Navbars/NavbarLight";
 import {FaTimes, FaCheck} from "react-icons/fa";
 
 export default class SignupTourPage2 extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentBtn: 1
+        }
+    }
     endOfFree() {
         let today = new Date();
         today.setDate(today.getDate() + 29);
@@ -13,82 +19,104 @@ export default class SignupTourPage2 extends Component {
         let endOfFree = `${month}/${day}/${year}`;
         return `Monthly price after free month ends on ${endOfFree}`;
     }
+    selectColumn = (e) => {
+        e.preventDefault();
+        const btn = e.currentTarget.dataset.id;
+        this.setState({currentBtn: btn});
+    }
     render() {
         const endOfFree = this.endOfFree();
+        const {currentBtn} = this.state;
+        console.log(currentBtn);
+        const btnSelectionActive = "btn-selection btn-active";
+        const btnSelectionInactive = "btn-selection";
+        const activeColumn = {color: "#E50914"}
         return (
-            <div className="signup-tour-container">
-                <NavbarLight logoLink="/" btnText="Sign In" btnLink="/signin-existing"/>
-                <form className="signup-page-2-inner">
-                    <div className="signup-page-2-top">
-                        <p className="step-number">
-                            Step<strong> 1 </strong>of<strong> 3 </strong>
-                        </p>
-                        <p className="text-xl text-bold">
-                            Choose the plan that's right for you</p>
-                        <p>Downgrade or upgrade at any time</p>
-                    </div>
-                    <table className="signup-table">
-                        <colgroup>
-                            <col span="1" id="col-1"/>
-                            <col span="1" className="col-2"/>
-                            <col span="1" className="col-3"/>
-                            <col span="1" className="col-4"/>
-                        </colgroup>
-                        <thead>
-                            <tr className="plan-selection-buttons">
-                                <th></th>
-                                <th className="btn-selection">Basic</th>
-                                <th className="btn-selection">Standard</th>
-                                <th className="btn-selection">Premium</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>{endOfFree}</td>
-                                <td>$8.99</td>
-                                <td>$12.99</td>
-                                <td>$15.99</td>
-                            </tr>
-                            <tr>
-                                <td>HD available</td>
-                                <td><FaTimes/></td>
-                                <td><FaCheck/></td>
-                                <td><FaCheck/></td>
-                            </tr>
-                            <tr>
-                                <td>Ultra HD available</td>
-                                <td><FaTimes/></td>
-                                <td><FaTimes/></td>
-                                <td><FaCheck/></td>
-                            </tr>
-                            <tr>
-                                <td>Screens you can watch on at the same time</td>
-                                <td>1</td>
-                                <td>2</td>
-                                <td>4</td>
-                            </tr>
-                            <tr>
-                                <td>Watch on your laptop, TV, phone and tablet</td>
-                                <td><FaCheck/></td>
-                                <td><FaCheck/></td>
-                                <td><FaCheck/></td>
-                            </tr>
-                            <tr>
-                                <td>Cancel anytime</td>
-                                <td><FaCheck/></td>
-                                <td><FaCheck/></td>
-                                <td><FaCheck/></td>
-                            </tr>
-                            <tr>
-                                <td>First month free</td>
-                                <td><FaCheck/></td>
-                                <td><FaCheck/></td>
-                                <td><FaCheck/></td>
-                            </tr>
-                        </tbody>
-                    </table>
-                    <Link to="/signup-tour/account-creation" className="btn btn-lg">Continue</Link>
-                </form>
+            <div>
+                <div className="white-background"></div>
+                <div className="signup-tour-container">
+                    <NavbarLight logoLink="/" btnText="Sign In" btnLink="/signin-existing"/>
+                    <form className="signup-page-2-inner">
+                        <div className="signup-page-2-top">
+                            <p className="step-number">
+                                Step <strong>1</strong> of <strong>3</strong>
+                            </p>
+                            <p className="text-xl text-bold">
+                                Choose the plan that's right for you</p>
+                            <p>Downgrade or upgrade at any time</p>
+                        </div>
+                        <table className="signup-table">
+                            <thead>
+                                <tr className="plan-selection-buttons">
+                                    <th></th>
+                                    <th
+                                        onClick={this.selectColumn}
+                                        data-id="btn-1"
+                                        className={currentBtn === "btn-1"
+                                        ? btnSelectionActive
+                                        : btnSelectionInactive}>Basic</th>
+                                    <th
+                                        onClick={this.selectColumn}
+                                        data-id="btn-2"
+                                        className={currentBtn === "btn-2"
+                                        ? btnSelectionActive
+                                        : btnSelectionInactive}>Standard</th>
+                                    <th
+                                        onClick={this.selectColumn}
+                                        data-id="btn-3"
+                                        className={currentBtn === "btn-3"
+                                        ? btnSelectionActive
+                                        : btnSelectionInactive}>Premium</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>{endOfFree}</td>
+                                    <td style={currentBtn === "btn-1" ? activeColumn : {}}>$8.99</td>
+                                    <td style={currentBtn === "btn-2" ? activeColumn : {}}>$12.99</td>
+                                    <td style={currentBtn === "btn-3" ? activeColumn : {}}>$15.99</td>
+                                </tr>
+                                <tr>
+                                    <td>HD available</td>
+                                    <td style={currentBtn === "btn-1" ? activeColumn : {}}><FaTimes/></td>
+                                    <td style={currentBtn === "btn-2" ? activeColumn : {}}><FaCheck/></td>
+                                    <td style={currentBtn === "btn-3" ? activeColumn : {}}><FaCheck/></td>
+                                </tr>
+                                <tr>
+                                    <td>Ultra HD available</td>
+                                    <td style={currentBtn === "btn-1" ? activeColumn : {}}><FaTimes/></td>
+                                    <td style={currentBtn === "btn-2" ? activeColumn : {}}><FaTimes/></td>
+                                    <td style={currentBtn === "btn-3" ? activeColumn : {}}><FaCheck/></td>
+                                </tr>
+                                <tr>
+                                    <td>Screens you can watch on at the same time</td>
+                                    <td style={currentBtn === "btn-1" ? activeColumn : {}}>1</td>
+                                    <td style={currentBtn === "btn-2" ? activeColumn : {}}>2</td>
+                                    <td style={currentBtn === "btn-3" ? activeColumn : {}}>4</td>
+                                </tr>
+                                <tr>
+                                    <td>Watch on your laptop, TV, phone and tablet</td>
+                                    <td style={currentBtn === "btn-1" ? activeColumn : {}}><FaCheck/></td>
+                                    <td style={currentBtn === "btn-2" ? activeColumn : {}}><FaCheck/></td>
+                                    <td style={currentBtn === "btn-3" ? activeColumn : {}}><FaCheck/></td>
+                                </tr>
+                                <tr>
+                                    <td>Cancel anytime</td>
+                                    <td style={currentBtn === "btn-1" ? activeColumn : {}}><FaCheck/></td>
+                                    <td style={currentBtn === "btn-2" ? activeColumn : {}}><FaCheck/></td>
+                                    <td style={currentBtn === "btn-3" ? activeColumn : {}}><FaCheck/></td>
+                                </tr>
+                                <tr>
+                                    <td>First month free</td>
+                                    <td style={currentBtn === "btn-1" ? activeColumn : {}}><FaCheck/></td>
+                                    <td style={currentBtn === "btn-2" ? activeColumn : {}}><FaCheck/></td>
+                                    <td style={currentBtn === "btn-3" ? activeColumn : {}}><FaCheck/></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <Link to="/signup-tour/account-creation" className="btn btn-lg">Continue</Link>
+                    </form>
+                </div>
             </div>
         )
     }
