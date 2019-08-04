84cf5a741914c9c4e874b6d78ba7c7c2d7f75613
ChadMcCaulley <cha2168871@maricopa.edu>
2019-08-03 21:32:59 -0700

    added signup form

--- src/pages/SignupForm.js
+++ src/pages/SignupForm.js
@@ -2,7 +2,6 @@ import React from 'react';
 import {Link} from "react-router-dom";
 import NavbarLight from "../components/Navbars/NavbarLight";
 
-import {FaCheckCircle, FaCheck} from "react-icons/fa";
 
 const SignupForm = () => {
     return (
@@ -10,9 +9,9 @@ const SignupForm = () => {
             <div className="white-background"></div>
             <div className="signup-tour-container">
                 <NavbarLight logoLink="/" btnText="Sign In" btnLink="/signin-existing"/>
-                <form className="signup-tour-inner">
-                    <div className="signup-tour-inner-top">
-                        <FaCheckCircle id="check-icon-circle" size={42}/>
+                <form className="signup-page-2-inner">
+                    <div className="signup-page-2-top">
+                        <p className="btn-blue">Enjoy your first month. <strong>It's free</strong></p>
                         <p className="step-number">
                             Step <strong>
                                 2
@@ -21,10 +20,18 @@ const SignupForm = () => {
                             </strong>
                         </p>
                         <p className="text-xl text-bold">Sign up to start your free month</p>
-                        <p className="text-black text-md text-no-margin"> Just two more steps and your done! <br/> We hate paperwork, too.</p>
+                        <p className="text-black text-no-margin"> Just two more steps and your done! <br/> We hate paperwork, too.</p>
                         <p className="text-black text-md text-bold"> Create your account.</p>
                     </div>
-                    <Link to="/signup-tour/plans" className="btn btn-lg">see the plans</Link>
+                    <div className="input-field-container">
+                            <label for="user_login_email" className="text-xsm">Email or phone number</label>
+                            <input type="text" id="user_login_email"></input>
+                        </div>
+                        <div className="input-field-container">
+                            <label for="user_login_password" className="text-xsm">Password</label>
+                            <input type="password" id="user_login_password"></input>
+                        </div>
+                    <Link to="/signup-tour/plans" className="btn btn-lg" style={{width: "100%"}}>continue</Link>
                 </form>
             </div>
         </div>
